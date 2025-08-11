b7e01a96ac0f74a8d470ad2ce1e6c95d5c33c5e9
VictorCottar <victorcottarms@outlook.com>
2025-08-11 19:54:46 -0300

    Página de Histórico de Fretes

---
+++ app/Models/Frete.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Frete extends Model
+{
+    public function etapas()
+    {
+        return $this->hasMany(Etapa::class);
+    }
+}
